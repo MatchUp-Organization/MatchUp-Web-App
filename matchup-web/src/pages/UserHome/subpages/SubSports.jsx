b375ef778c166df020839733c934daf566750873
Tiago Martins <tiago.martins-sousa@epitech.eu>
2024-07-19 05:11:24 +0200

    fdg

--- matchup-web/src/pages/UserHome/subpages/SubSports.jsx
+++ matchup-web/src/pages/UserHome/subpages/SubSports.jsx
@@ -1,19 +1,50 @@
 import React from 'react';
+import MatchUpLogo from "../../../assets/MatchUp-Logo.svg";
+import HomeLogo from "../../../assets/dashboard-home.svg";
+import CalendarLogo from "../../../assets/dashboard-calendar.svg";
+import SportsLogo from "../../../assets/dashboard-sports.svg";
+import SettingsLogo from "../../../assets/dashboard-settings.svg";
+import NoProfilePictureLogo from "../../../assets/dashboard-noprofilepicture.svg";
+import SearchLogo from "../../../assets/dashboard-search.svg";
 import './SubSports.css';
 import MatchListItem from '../../../components/ui/MatchListItem/MatchListItem';
 
 const SubSports = () => {
   return (
-    <div className="full-height-width">
-      Sports
-      <MatchListItem
-        img_url="https://via.placeholder.com/500"
-        title="Football Match"
-        location="Football Field"
-        date="19/07/2024"
-        users="18/22"
-      />
-    </div>
+        <div className="AllSportsPage">
+            <div className="section" style={{ backgroundImage: `url('./basketball.png')` }}>
+                <div className="sport-name">Basketball</div>
+                <div className="section-content">
+                    <p>Players looking for a match: {Math.floor(Math.random() * 100)}</p>
+                    <p>Matches looking for players: {Math.floor(Math.random() * 50)}</p>
+                    <button className="join-up-button">Join Up</button>
+                </div>
+            </div>
+            <div className="section" style={{ backgroundImage: `url('./football.png')` }}>
+                <div className="sport-name">Football</div>
+                <div className="section-content">
+                    <p>Players looking for a match: {Math.floor(Math.random() * 100)}</p>
+                    <p>Matches looking for players: {Math.floor(Math.random() * 50)}</p>
+                    <button className="join-up-button">Join Up</button>
+                </div>
+            </div>
+            <div className="section" style={{ backgroundImage: `url('./volleyball.png')` }}>
+                <div className="sport-name">Volleyball</div>
+                <div className="section-content">
+                    <p>Players looking for a match: {Math.floor(Math.random() * 100)}</p>
+                    <p>Matches looking for players: {Math.floor(Math.random() * 50)}</p>
+                    <button className="join-up-button">Join Up</button>
+                </div>
+            </div>
+            <div className="section" style={{ backgroundImage: `url('./tennis.png')` }}>
+                <div className="sport-name">Tennis</div>
+                <div className="section-content">
+                    <p>Players looking for a match: {Math.floor(Math.random() * 100)}</p>
+                    <p>Matches looking for players: {Math.floor(Math.random() * 50)}</p>
+                    <button className="join-up-button">Join Up</button>
+                </div>
+            </div>
+        </div>
   );
 };
 
