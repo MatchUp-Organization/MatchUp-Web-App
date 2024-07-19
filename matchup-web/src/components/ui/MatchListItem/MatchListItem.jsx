8cda8d87407db170f2491bd09bb5c9c2b5f3ac4d
Zenwoh <vitor.reis4500@gmail.com>
2024-07-19 04:54:03 +0200

    Added match component

--- matchup-web/src/components/ui/MatchListItem/MatchListItem.jsx
+++ matchup-web/src/components/ui/MatchListItem/MatchListItem.jsx
@@ -1,19 +1,27 @@
 import './MatchListItem.css';
+import LocationPin from '../../../assets/location-pin.svg';
+import UsersLogo from '../../../assets/users-logo.svg';
 
-export default function MatchListItem() {
+export default function MatchListItem({ img_url, title, location, date, users }) {
     return (
         <div className="matchlistitem-container">
             <div className="matchlistitem-header">
-                <img src="https://via.placeholder.com/500" alt="Field Image" className="matchlistitem-header_image" />
+                <img src={img_url} alt="Field Image" className="matchlistitem-header_image" />
             </div>
             <div className="matchlistitem-content">
                 <div className="matchlistitem-content-left">
-                    <p className="matchlistitem-content-left_title">Match Title</p>
-                    <p className="matchlistitem-content-left_description">Match Location</p>
+                    <p className="matchlistitem-content-left_title">{title}</p>
+                    <div className="matchlistitem-content-left_location">
+                        <img src={LocationPin} alt="Location Pin" className="matchlistitem-content-left_location-image" />
+                        <p className="matchlistitem-content-left_location-text">{location}</p>
+                    </div>
                 </div>
                 <div className="matchlistitem-content-right">
-                    <p className="matchlistitem-content-right_title">Match Date</p>
-                    <p className="matchlistitem-content-right_description">Player Count</p>
+                    <p className="matchlistitem-content-right_date">{date}</p>
+                    <div className="matchlistitem-content-right_users">
+                        <img src={UsersLogo} alt="Users Logo" className="matchlistitem-content-right_users-image" />
+                        <p className="matchlistitem-content-right_users-text">{users}</p>
+                    </div>
                 </div>
             </div>
         </div>
--- matchup-web/src/pages/UserHome/subpages/SubSports.jsx
+++ matchup-web/src/pages/UserHome/subpages/SubSports.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './SubSports.css';
+import MatchListItem from '../../../components/ui/MatchListItem/MatchListItem';
 
 const SubSports = () => {
   return (
     <div className="full-height-width">
       Sports
+      <MatchListItem
+        img_url="https://via.placeholder.com/500"
+        title="Football Match"
+        location="Football Field"
+        date="19/07/2024"
+        users="18/22"
+      />
     </div>
   );
 };
